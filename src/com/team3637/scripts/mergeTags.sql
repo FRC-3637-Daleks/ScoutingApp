764d7df064832f2ffc426b542de3dc44314bddae
zethra <jediben97@gmail.com>
2015-09-30 19:03:43 -0400

    Added merge tags sql script

---
+++ src/com/team3637/scripts/mergeTags.sql
@@ -0,0 +1,19 @@
+DROP PROCEDURE IF EXISTS mergeTags|
+CREATE PROCEDURE mergeTags(
+  IN tableName VARCHAR(20),
+  IN oldTag VARCHAR(45),
+  IN newTag VARCHAR(45)
+)
+  BEGIN
+    SET @cols = (SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'scoutingtags' AND table_name = 'matches') - 4;
+    #SELECT @cols;
+    SET @i = 0;
+    WHILE @i < @cols DO
+      #SELECT @i;
+      SET @makeCol = CONCAT('UPDATE ',tableName , ' SET `tag', @i, '`=\'', newTag,'\' WHERE `tag', @i, '`=\'', oldTag, '\'');
+      PREPARE stmt FROM @makeCol;
+      EXECUTE stmt;
+      DEALLOCATE PREPARE stmt;
+      SET @i = @i + 1;
+    END WHILE;
+  END |
