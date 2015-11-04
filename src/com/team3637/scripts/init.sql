1600cbfa38b6f2da5aa53e305f384a7bdb6467b4
zethra <jediben97@gmail.com>
2015-11-03 19:35:32 -0500

    Added sql init script

---
+++ src/com/team3637/scripts/init.sql
@@ -0,0 +1,101 @@
+DROP PROCEDURE IF EXISTS init|
+CREATE PROCEDURE init()
+  BEGIN
+    CREATE TABLE IF NOT EXISTS matches (
+      id       INT NOT NULL,
+      matchNum INT NOT NULL,
+      team     INT NOT NULL,
+      score    INT NOT NULL,
+      tag0     VARCHAR(45),
+      tag1     VARCHAR(45),
+      tag2     VARCHAR(45),
+      tag3     VARCHAR(45),
+      tag4     VARCHAR(45),
+      tag5     VARCHAR(45),
+      tag6     VARCHAR(45),
+      tag7     VARCHAR(45),
+      PRIMARY KEY (id, matchNum, team)
+    );
+
+    IF (SELECT COUNT(INDEX_NAME)
+        FROM INFORMATION_SCHEMA.STATISTICS
+        WHERE
+          `TABLE_CATALOG` = 'def' AND `TABLE_SCHEMA` = DATABASE() AND
+          `TABLE_NAME` = 'matches' AND `INDEX_NAME` = 'id_UNIQUE') = 0
+    THEN
+      CREATE UNIQUE INDEX id_UNIQUE ON matches (id);
+    END IF;
+
+    CREATE TABLE IF NOT EXISTS schedule (
+      id       INT NOT NULL,
+      matchNum INT,
+      b1       INT,
+      b2       INT,
+      b3       INT,
+      r1       INT,
+      r2       INT,
+      r3       INT,
+      PRIMARY KEY (id)
+    );
+    IF (SELECT COUNT(INDEX_NAME)
+        FROM INFORMATION_SCHEMA.STATISTICS
+        WHERE
+          `TABLE_CATALOG` = 'def' AND `TABLE_SCHEMA` = DATABASE() AND
+          `TABLE_NAME` = 'schedule' AND `INDEX_NAME` = 'id_UNIQUE') = 0
+    THEN
+      CREATE UNIQUE INDEX id_UNIQUE ON schedule (id);
+    END IF;
+
+    CREATE TABLE IF NOT EXISTS  tags (
+      id   INT         NOT NULL,
+      tag  VARCHAR(45) NOT NULL,
+      type VARCHAR(45) NOT NULL,
+      PRIMARY KEY (id, tag)
+    );
+    IF (SELECT COUNT(INDEX_NAME)
+        FROM INFORMATION_SCHEMA.STATISTICS
+        WHERE
+          `TABLE_CATALOG` = 'def' AND `TABLE_SCHEMA` = DATABASE() AND
+          `TABLE_NAME` = 'tags' AND `INDEX_NAME` = 'id_UNIQUE') = 0
+    THEN
+      CREATE UNIQUE INDEX id_UNIQUE ON tags (id);
+    END IF;
+    IF (SELECT COUNT(INDEX_NAME)
+        FROM INFORMATION_SCHEMA.STATISTICS
+        WHERE
+          `TABLE_CATALOG` = 'def' AND `TABLE_SCHEMA` = DATABASE() AND
+          `TABLE_NAME` = 'tags' AND `INDEX_NAME` = 'name_UNIQUE') = 0
+    THEN
+      CREATE UNIQUE INDEX name_UNIQUE ON tags (tag);
+    END IF;
+
+    CREATE TABLE IF NOT EXISTS  teams (
+      id       INT         NOT NULL,
+      team     INT         NOT NULL,
+      avgscore FLOAT(6, 2) NOT NULL DEFAULT 0.00,
+      matches  INT         NOT NULL DEFAULT 0,
+      tag0     VARCHAR(45),
+      tag1     VARCHAR(45),
+      tag2     VARCHAR(45),
+      tag3     VARCHAR(45),
+      tag4     VARCHAR(45),
+      PRIMARY KEY (id, team)
+    );
+    IF (SELECT COUNT(INDEX_NAME)
+        FROM INFORMATION_SCHEMA.STATISTICS
+        WHERE
+          `TABLE_CATALOG` = 'def' AND `TABLE_SCHEMA` = DATABASE() AND
+          `TABLE_NAME` = 'teams' AND `INDEX_NAME` = 'id_UNIQUE') = 0
+    THEN
+      CREATE UNIQUE INDEX id_UNIQUE ON teams (id);
+    END IF;
+    IF (SELECT COUNT(INDEX_NAME)
+        FROM INFORMATION_SCHEMA.STATISTICS
+        WHERE
+          `TABLE_CATALOG` = 'def' AND `TABLE_SCHEMA` = DATABASE() AND
+          `TABLE_NAME` = 'teams' AND `INDEX_NAME` = 'team_UNIQUE') = 0
+    THEN
+      CREATE UNIQUE INDEX team_UNIQUE ON teams (team);
+    END IF;
+  END |
+CALL init()|
